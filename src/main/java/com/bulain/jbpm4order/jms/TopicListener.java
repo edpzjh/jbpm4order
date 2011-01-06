05dd4883b6d05e85a9b41fba2c7ed6a1bc70a92b
Bulain <bulain@gmail.com>
2011-01-07 00:01:16 +0800

    add jms topi

---
+++ src/main/java/com/bulain/jbpm4order/jms/TopicListener.java
@@ -0,0 +1,7 @@
+package com.bulain.jbpm4order.jms;
+
+public class TopicListener {
+	public void onMessage(String body) {
+		System.out.println("Topic: " + body);
+	}
+}
---
+++ src/test/java/com/bulain/jbpm4order/integration/jms/TopicPublicerTest.java
@@ -0,0 +1,26 @@
+package com.bulain.jbpm4order.integration.jms;
+
+import org.springframework.jms.core.JmsTemplate;
+
+import com.bulain.common.ServiceTestCase;
+
+public class TopicPublicerTest extends ServiceTestCase {
+	private JmsTemplate jmsTemplateC;
+	
+	public static void main(String[] args) {
+		junit.textui.TestRunner.run(TopicPublicerTest.class);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		jmsTemplateC = (JmsTemplate) applicationContext.getBean("jmsTemplateC");
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testPublic(){
+		jmsTemplateC.convertAndSend("This is a public Message!");
+	}
+}
